a5da4380da61998e31127ff6bbef02c241f9c1cd
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-19 21:44:53 +0900

    画像の保存を許可するストロングパラメーターを記述

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -1,4 +1,10 @@
 class PhotoController < ApplicationController
   def index
   end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :text, :date, :image).merge(user_id, :currebt.id)
+  end
 end
