9853498c467f6bf979e05acc5c224b82d004d85c
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-20 18:07:11 +0900

    photoが保存されればトップページへ、保存されなければ投稿ページへ遷移する記述

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,8 @@
 class Photo < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :title, presence: true
+  validates :text,  presence: true
+  validates :date,  presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "photo#index"
+  resources :photo, only: [:index, :new, :create]
 end
