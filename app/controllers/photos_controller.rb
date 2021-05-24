4d55ae74b4fca9792757eb9e89016cc6356f9c45
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-24 18:44:21 +0900

    テスト

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,8 +7,7 @@ class PhotosController < ApplicationController
   end
 
   def create
-    # binding.pry
-    Photo.create(photo_params) #createメソッドの引数に使用して、photosテーブルへ保存できるよう
+    # Photo.create(photo_params) #createメソッドの引数に使用して、photosテーブルへ保存できるよう
     @photo = Photo.new(photo_params)
     if @photo.save
       redirect_to root_path
