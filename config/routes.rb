66914c19d2037215ceeb43072408faefdf88007a
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-27 15:39:00 +0900

    7つのアクションを短縮

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "photos#index"
-  resources :photos, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :messages, only: [:index, :create]
+  resources :photos
+  resources :messages, only: [:index, :create, :new]
 end
